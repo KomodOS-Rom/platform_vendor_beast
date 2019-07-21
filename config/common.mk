mirdaVG/platform_vendor_aosip

Add Lawnchair as default launcher
- CI build Lawnchair-alpha_839
- Thanks @deletescape :)

Change-Id: Ia31a36cf899a5244536a24ca963661317b2f9146
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

config/common.mk
@@ -86,6 +86,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
+# Lawnchair
+PRODUCT_PACKAGES += Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/aosip/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 

KomodOS-Rom/platform_vendor_beast

config/common.mk
@@ -54,6 +54,12 @@ DEVICE_PACKAGE_OVERLAYS += \
     vendor/komodo/overlay/common \
     vendor/komodo/overlay/themes
 
+# Lawnchair
+PRODUCT_PACKAGES += Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/komodo/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 # Bootanimation
 $(call inherit-product, vendor/komodo/config/bootanimation.mk)
 